108c7786ac4cd1e11193966497ccb0a696fee58e
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-01-28 14:58:32 +0100

    Complete If/Else Exercice

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -7,27 +7,39 @@ public class WormController : MonoBehaviour
 
     public int x;
     public string text;
+    public float number;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (x > 0)
+        /* if (x > 0)
+         {
+             Debug.Log("x ist größer als 0");
+         }
+         else
+         {
+             Debug.Log("x ist kleiner als 0");
+         }
+
+
+         if (text == "OwO")
+         {
+             Debug.Log("OwO what's this? Its Weeb o' clock");
+         }
+         else
+         {
+             Debug.Log("This does not have OWO Energy");
+         } */
+
+        if (number > 10)
         {
-            Debug.Log("x ist größer als 0");
+            Debug.Log("Nummer ist größer als 10");
         }
         else
         {
-            Debug.Log("x ist kleiner als 0");
+            Debug.Log("Nummer ist kleiner als 10");
         }
 
-        if (text == "OwO")
-        {
-            Debug.Log("OwO what's this? Its Weeb o' clock");
-        }
-        else
-        {
-            Debug.Log("This does not have OWO Energy");
-        }
     }
 
     // Update is called once per frame
