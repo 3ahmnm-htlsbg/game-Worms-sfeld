7df037e9359922af33ee587ff2df4acb2925ea00
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-01-28 15:08:33 +0100

    add getKeyDown

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -4,47 +4,11 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-
-    public int x;
-    public string text;
-    public float number;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        /* if (x > 0)
-         {
-             Debug.Log("x ist größer als 0");
-         }
-         else
-         {
-             Debug.Log("x ist kleiner als 0");
-         }
-
-
-         if (text == "OwO")
-         {
-             Debug.Log("OwO what's this? Its Weeb o' clock");
-         }
-         else
-         {
-             Debug.Log("This does not have OWO Energy");
-         } */
-
-        if (number > 10)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("Nummer ist größer als 10");
+            Debug.Log("Wird aufgerufen!");
         }
-        else
-        {
-            Debug.Log("Nummer ist kleiner als 10");
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
