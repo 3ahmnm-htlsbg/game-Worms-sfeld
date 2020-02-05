9df23c35bcad98f9d4b29f70ffd7ac660a00766b
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-02-05 07:57:20 +0100

    Add left, right and upward movement as well as a functions to shoot projectiles to worm controller Script

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -4,11 +4,52 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
+    public float strength;
+    public float ballStength;
+    public GameObject Worm;
+    public GameObject Ball;
+    public KeyCode RightKey;
+    public KeyCode LeftKey;
+    public KeyCode JumpKey;
+    public KeyCode TriggerKey;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        Rigidbody WormRigidbody = Worm.gameObject.GetComponent<Rigidbody>();
+
+        if (Input.GetKeyDown(RightKey))
         {
-            Debug.Log("Wird aufgerufen!");
+           
+            Debug.Log("Move Right");
+
+            WormRigidbody.AddForce(transform.right*strength, ForceMode.Acceleration);
         }
+        if (Input.GetKeyDown(LeftKey))
+        {
+            Debug.Log("Move Left");
+
+            WormRigidbody.AddForce(-transform.right * strength, ForceMode.Acceleration);
+        }
+        if (Input.GetKeyDown(JumpKey))
+        {
+            Debug.Log("Move Up");
+
+            WormRigidbody.AddForce(transform.up * strength, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKeyDown(TriggerKey))
+        {
+            ShootBall();
+        }
+
+    }
+
+    private void ShootBall()
+    {
+        
+        Vector3 ballPos = Worm.transform.position + new Vector3(1, 0, 0);
+        GameObject newball = Instantiate(Ball, ballPos, Quaternion.identity);
+        Rigidbody ballRigid = newball.gameObject.GetComponent<Rigidbody>();
+        ballRigid.AddForce(transform.right * ballStength);
     }
 }
