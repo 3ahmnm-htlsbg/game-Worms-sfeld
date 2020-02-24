233d01673b48aa2df061d1f312908e7f327f0084
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-02-24 20:50:59 +0100

    modify movement and add roation to basuka

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -5,51 +5,132 @@ using UnityEngine;
 public class WormController : MonoBehaviour
 {
     public float strength;
-    public float ballStength;
-    public GameObject Worm;
-    public GameObject Ball;
-    public KeyCode RightKey;
-    public KeyCode LeftKey;
-    public KeyCode JumpKey;
-    public KeyCode TriggerKey;
+    public float bulletForce;
+    public GameObject worm;
+    public GameObject ball;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+    public KeyCode jumpKey;
+    public KeyCode triggerKey;
+    public KeyCode gunUp;
+    public KeyCode gunDown;
+
+    float speed = 5f;
 
     private void Update()
     {
-        Rigidbody WormRigidbody = Worm.gameObject.GetComponent<Rigidbody>();
+        
+        Rigidbody WormRigidbody = worm.gameObject.GetComponent<Rigidbody>();
 
-        if (Input.GetKeyDown(RightKey))
-        {
-           
-            Debug.Log("Move Right");
+        worm.transform.position += MovePlayer();
+        Quaternion rotation = Quaternion.Euler(0, 0, rotateGun());
+        worm.transform.GetChild(0).gameObject.transform.rotation *= rotation;
 
-            WormRigidbody.AddForce(transform.right*strength, ForceMode.Acceleration);
-        }
-        if (Input.GetKeyDown(LeftKey))
-        {
-            Debug.Log("Move Left");
-
-            WormRigidbody.AddForce(-transform.right * strength, ForceMode.Acceleration);
-        }
-        if (Input.GetKeyDown(JumpKey))
-        {
-            Debug.Log("Move Up");
-
-            WormRigidbody.AddForce(transform.up * strength, ForceMode.Acceleration);
-        }
-
-        if (Input.GetKeyDown(TriggerKey))
+        //moveWithForce(WormRigidbody);
+        Jump(WormRigidbody);
+        if (Input.GetKeyDown(triggerKey))
         {
             ShootBall();
         }
-
+        
     }
 
     private void ShootBall()
     {
-        
-        Vector3 ballPos = Worm.transform.position + new Vector3(1, 0, 0);
-        GameObject newball = Instantiate(Ball, ballPos, Quaternion.identity);
+        GameObject Gun = worm.transform.GetChild(0).GetChild(1).gameObject;
+        GameObject gunPivot = worm.transform.GetChild(0).gameObject;
+
+        GameObject newball = Instantiate(ball, Gun.transform.position, Quaternion.identity);
         Rigidbody ballRigid = newball.gameObject.GetComponent<Rigidbody>();
-        ballRigid.AddForce(transform.right * ballStength);
+        Vector3 bulletDirection = new Vector3((worm.transform.rotation.eulerAngles.y/ 90) - 1, 0, gunPivot.transform.rotation.eulerAngles.z / 180);
+        Debug.Log("bullet direction: " + bulletDirection + "current Rotation: " + worm.transform.rotation.eulerAngles);
+        ballRigid.AddForce(bulletDirection * bulletForce);
+    }
+
+    void Jump(Rigidbody rigid)
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Debug.Log("Move Up");
+
+            rigid.AddForce(transform.up * strength, ForceMode.Acceleration);
+        }
+    }
+
+    void moveWithForce(Rigidbody rigid)
+    {
+        if (Input.GetKey(rightKey))
+        {
+
+            Debug.Log("Move Right");
+
+            rigid.AddForce(transform.right * strength, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(leftKey))
+        {
+            Debug.Log("Move Left");
+
+            rigid.AddForce(-transform.right * strength, ForceMode.Acceleration);
+        }
+    }
+
+    Vector3 MovePlayer()
+    {
+        if (Input.GetKey(rightKey))
+        {
+            Vector3 input = new Vector3(1, 0, 0);
+            Vector3 direction = input.normalized;
+            Vector3 velocity = direction * speed;
+            Vector3 moveAmount = velocity * Time.deltaTime;
+            Debug.Log("Move Right by " + moveAmount);
+
+            worm.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            return moveAmount;
+        }
+        else if (Input.GetKey(leftKey))
+        {
+            
+            Vector3 input = new Vector3(-1, 0, 0);
+            Vector3 direction = input.normalized;
+            Vector3 velocity = direction * speed;
+            Vector3 moveAmount = velocity * Time.deltaTime;
+            Debug.Log("Move Left by " + moveAmount);
+
+            worm.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            return moveAmount;
+            
+        }
+        else
+        {
+            return new Vector3(0, 0, 0);
+        }
+    }
+
+    float rotateGun()
+    {
+        GameObject Gun = worm.transform.GetChild(0).gameObject;
+
+        if (Input.GetKey(gunUp)) {
+
+            float rotationSpeed = 180f;
+            float roationAmount = rotationSpeed * Time.deltaTime;
+
+            return roationAmount;
+        }
+        if (Input.GetKey(gunDown))
+        {
+
+            float rotationSpeed = -180f;
+            float roationAmount = rotationSpeed * Time.deltaTime;
+
+            return roationAmount;
+        }
+        else
+        {
+            return 0f;
+        }
+
     }
 }
