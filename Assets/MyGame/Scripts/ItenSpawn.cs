8dfa9c6e9f0f7e19349deced793ff6269545ed4b
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-06-23 14:57:07 +0200

    Update Game and Graphics

---
+++ Assets/MyGame/Scripts/ItenSpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItenSpawn : MonoBehaviour
+{
+
+    public Transform[] transforms;
+    public GameObject[] itemRef;
+    public WormController wormController;
+
+    private  List<Vector3> positions = new List<Vector3>() ;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        for (int i = 0; i < transforms.Length ; i++)
+        {
+            positions.Add(transforms[i].position);
+        }
+    }
+
+    private void Update()
+    {
+        print(WormController.itemAmmount);
+
+        if(WormController.itemAmmount <= 0)
+        {
+            StartCoroutine(SpawnItems());
+            print("coroutine Started");
+        }
+
+        if(WormController.itemAmmount >= 4)
+        {
+            StopAllCoroutines();
+            print("coroutine Stopped");
+        }
+
+    }
+
+
+    IEnumerator SpawnItems()
+    {
+        yield return new WaitForSeconds(2f);
+
+        int itemIndex = Random.Range(0, itemRef.Length );
+        int position = Random.Range(0, positions.Count );
+
+        Instantiate(itemRef[itemIndex], positions[position], Quaternion.identity);
+        WormController.itemAmmount++;
+
+        yield return new WaitForSeconds(5f);
+        yield return StartCoroutine(SpawnItems());
+        
+    }
+   
+}
--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WormController : MonoBehaviour
 {
+    public int playerIndex;
+
     public float strength;
     public float bulletForce;
     public float speed = 5f;
     public int WormHealth = 5;
+    private int startingHealth;
+    private float defaultSpeed;
 
+    public GameObject spawnPoint;
     public GameObject worm;
     public GameObject ball;
+    public GameObject Gun;
     public Text healthText;
+    public Text EndText;
+    public Animator animator;
 
     //Input Keys
     public KeyCode rightKey;
@@ -21,54 +30,74 @@ public class WormController : MonoBehaviour
     public KeyCode triggerKey;
     public KeyCode gunUp;
     public KeyCode gunDown;
-    
+
+    public static int itemAmmount = 4;
+
+    private void Start()
+    {
+        startingHealth = WormHealth;
+        defaultSpeed = speed;
+    }
 
     private void Update()
     {
         Rigidbody WormRigidbody = worm.gameObject.GetComponent<Rigidbody>();
+     
+        worm.transform.position += MovePlayer();  //moves player a set amount by vector mutiplicatin, as long as the move key is held down
 
-        //moves player a set amount by vector mutiplicatin, as long as the move key is held down
-        worm.transform.position += MovePlayer();
+        Jump(WormRigidbody); //jumping by adding upwards force if a key is pressed
 
-        //jumping by adding upwards force if a key is pressed
-        Jump(WormRigidbody);
-
-        //shoot bullet if gun is fired
-        if (Input.GetKeyDown(triggerKey))
+        if (Input.GetKeyDown(triggerKey)) 
         {
-            ShootBall();
+            ShootBall(); //shoot bullet if gun is fired
         }
-       
-        //rotate gun up or down
-        Quaternion rotation = Quaternion.Euler(0, 0, rotateGun());
-        worm.transform.GetChild(0).gameObject.transform.rotation *= rotation;
+           
+        Quaternion rotation = Quaternion.Euler(0, 0, rotateGun());  //rotate gun up or down
+        Gun.transform.parent.transform.rotation *= rotation;
 
         DisplayHealth();
+
+        if (WormHealth <= 0)
+        {
+            EndGame();
+        }
 
         //old method of moving player by adding a force everytime a move key is pressed
         //moveWithForce(WormRigidbody);
     }
 
-    
-
     private void OnCollisionEnter(Collision collision)
     {
+
+        if (collision.gameObject.tag == "PowerUp")
+        {
+            Debug.Log("Power UP");
+            bulletForce += 50;
+            itemAmmount--;
+            Destroy(collision.transform.parent.gameObject);
+            Destroy(collision.gameObject);            
+        }
+
         if (collision.gameObject.tag == "DamageSource")
         {
             Debug.Log("you got shot");
             WormHealth -= 1;
         }
+
         else if (collision.gameObject.tag == "RespawnZone")
         {
-            worm.transform.position = new Vector3(0, 0, 0);
+            worm.transform.position = spawnPoint.transform.position;
+            WormHealth -= 1;
             Debug.Log("you have fallen");
         }
+
         else if  (collision.gameObject.tag == "HelfItem") {
 
             Debug.Log("You just got healed");
             WormHealth += 5;
-            Destroy(collision.gameObject);
-            
+            itemAmmount--;
+            Destroy(collision.transform.parent.gameObject);
+            Destroy(collision.gameObject);         
         }
         else
         {
@@ -78,10 +107,10 @@ public class WormController : MonoBehaviour
     }
 
     private void ShootBall()
-    {   
+    {
         //get the required gameobjects
-        GameObject Gun = worm.transform.GetChild(0).GetChild(1).gameObject;
-        GameObject gunPivot = worm.transform.GetChild(0).gameObject;
+        //GameObject Gun = worm.transform.GetChild(0).GetChild(1).gameObject;
+        GameObject gunPivot = Gun.transform.parent.gameObject; ;  //worm.transform.GetChild(0).gameObject;
 
         //Insatntiate a new bullet and get its rigidbody
         GameObject newball = Instantiate(ball, Gun.transform.position, Quaternion.identity);
@@ -95,6 +124,13 @@ public class WormController : MonoBehaviour
         Debug.Log("bullet direction: " + bulletDirection + "current Rotation: " + gunPivot.transform.up);
 
         ballRigid.AddForce(bulletDirection * bulletForce);
+        StartCoroutine(DeleteBullet()); //starting a the corrutine after the bullet collides with the ground would probably be smarter but it's late and I don't feel like it. It is what it is 
+
+        IEnumerator DeleteBullet()
+        {
+            yield return new WaitForSeconds(2f);
+            Destroy(newball);         
+        }
     }
 
     void Jump(Rigidbody rigid)
@@ -139,6 +175,7 @@ public class WormController : MonoBehaviour
             //Set roation so the player looks in the direction they're moving in
             worm.transform.rotation = Quaternion.Euler(0, 180, 0);
 
+            animator.SetBool("moving", true);
             return moveAmount;
         }
 
@@ -153,11 +190,15 @@ public class WormController : MonoBehaviour
             //same deal as before
             worm.transform.rotation = Quaternion.Euler(0, 0, 0);
 
+            animator.SetBool("moving" ,true);
+
             return moveAmount;   
+
         }
 
         else
-        {       
+        {
+            animator.SetBool("moving", false);
             return new Vector3(0, 0, 0);
         }
     }
@@ -165,10 +206,10 @@ public class WormController : MonoBehaviour
     float rotateGun()
     {
         GameObject Gun = worm.transform.GetChild(0).gameObject;
-        // works like the MovePlayer function, just with roation instread
+        // works like the MovePlayer function, just with roation instead
         if (Input.GetKey(gunUp)) {
 
-            float rotationSpeed = 180f;
+            float rotationSpeed = 90f;
             float roationAmount = rotationSpeed * Time.deltaTime;
 
             return roationAmount;
@@ -192,5 +233,23 @@ public class WormController : MonoBehaviour
     {
         healthText.text = "Health: " + WormHealth.ToString();
 
+    }
+
+    public void EndGame()
+    {
+        EndText.text = "Player " + playerIndex + " lost";
+        GameObject endScreen = EndText.transform.parent.gameObject;
+        endScreen.SetActive(true);
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
